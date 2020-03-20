f75c2bebcb2fee382b0834eef6311c020724e62f
Tung <864109962@qq.com>
2020-03-20 20:45:34 +0800

    init

---
+++ CDLiveStreaming/CDLiveStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CDLiveStreaming
+//
+//  Created by Tung on 2020/3/20.
+//  Copyright © 2020年 chengdong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
