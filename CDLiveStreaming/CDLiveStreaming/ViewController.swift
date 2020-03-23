f8c78fbad07148b7567cd749574bdf26adf010c2
Tung <864109962@qq.com>
2020-03-23 21:05:45 +0800

    LFLiveKit

--- CDLiveStreaming/CDLiveStreaming/ViewController.swift
+++ CDLiveStreaming/CDLiveStreaming/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Getters and Setters
+    lazy var session: LFLiveSession = {
+        let audioConfiguration = LFLiveAudioConfiguration.defaultConfiguration()
+        let videoConfiguration = LFLiveVideoConfiguration.defaultConfigurationForQuality(LFLiveVideoQuality.Low3, landscape: false)
+        let session = LFLiveSession(audioConfiguration: audioConfiguration, videoConfiguration: videoConfiguration)
+        
+        session?.delegate = self
+        session?.preView = self.view
+        return session!
+    }()
+    
+    //MARK: - Event
+    func startLive() -> Void {
+        let stream = LFLiveStreamInfo()
+        stream.url = "your server rtmp url";
+        session.startLive(stream)
+    }
+    
+    func stopLive() -> Void {
+        session.stopLive()
+    }
+    
+    //MARK: - Callback
+    func liveSession(session: LFLiveSession?, debugInfo: LFLiveDebug?){
+        
+    }
+    func liveSession(session: LFLiveSession?, errorCode: LFLiveSocketErrorCode){
+        
+    }
+    func liveSession(session: LFLiveSession?, liveStateDidChange state: LFLiveState){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
